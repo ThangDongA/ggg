6be450066d756f6072b8b3f7dbbb373066fb5497
ThangDongA <111736761+ThangDongA@users.noreply.github.com>
2023-12-05 06:41:34 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Vợ yêu à :<',
+    introDesc: `C biết là chồng sai rồi khi để em khó chịu về chuyện sáng nay như vậy
+    Vợ đang bị áp lực học nữa đáng lẽ ra anh phải cẩn thận hơn bình thường `,
+    btnIntro: 'Tiếp nè',
+    title: 'Mình làm hòa nha vợ yêu 😙',
+    desc: ' Yêu vợ nhiều lắm ạ <33 ',
+    btnYes: 'Vợ đồng ý <33',
+    btnNo: 'Vợ không đồng ý hihi',
+    question:'Mình làm hòa nha vợ yêu !!!',
+    btnReply: 'Gửi lời muốn nói cho c nè ',
+    reply: ' Vợ tha lỗi cho c đó :v',
+    mess: 'Nếu em đến đoạn này rồi thì anh muốn nói anh yêu em rất nhiều. Thật sự anh không muốn em phải buồn phiền, phải suy nghĩ về anh như một áp lực. Anh biết tính em rất dễ khó chịu và nóng nên anh cũng đã rất cố gắng bình tĩnh để có thể bên em, em thấy điều đó mà đúng chứ. Anh đã quen với cuộc sống có em nên cảm xúc của anh phụ thuộc hết vào em rồi. Em khó chịu như này anh cũng chẳng dễ chịu hơn em là mấy :< Thật sự anh muốn chúng mình giải quyết vấn đề cùng nhau, anh không muốn thấy em ôm cả cái sự khó chịu ấy trong người mà phải suy nghĩ nhiều. Em có hỏi điều anh muốn em sửa là gì mà đúng không. Có chuyện gì hãy chia sẻ với anh, anh luôn ở bên vợ mà. Anh yêu em nhiều lắm <3 ',
+    messDesc: 'Nhắn tin cho c nhe vợ :>>.',
+    btnAccept: 'Âu kây :3',
+    messLink: 'https://www.messenger.com/t/100031669620788' //link mess của các bạn. VD: https://m.me/nam.nodemy 100031669620788
+}
